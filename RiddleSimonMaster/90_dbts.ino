c234e5d2075e5b5a50509e7143b28ffbcecbe7b0
CharlieSaxon <matthew.t.charles@gmail.com>
2022-07-14 14:14:43 -0500

    Update 90_dbts.ino
    basic serial monitor feedback

--- RiddleSimonMaster/90_dbts.ino
+++ RiddleSimonMaster/90_dbts.ino
@@ -1,5 +1,16 @@
 void dbts(){
 
   bool somethingNew = false;
-
+  for (int reg = 0; reg < numPISOregs; reg++){
+    if (regByteNew[reg] != regByteOld[reg]){
+      somethingNew = true;
+    }
+  }
+  if (somethingNew){
+    Serial.print("puzzle: ");
+    Serial.print(regByteNew[1],BIN);
+    Serial.print("/tsimon: ");
+    Serial.print(regByteNew[0],BIN);
+    Serial.println();
+  }
 }
